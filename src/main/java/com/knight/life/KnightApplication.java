6a6eb2a52d717eb4fee18d1364e3416fbbbd88ac
Administrator <yerongjie1610@163.com>
2019-07-29 17:25:42 +0800

    first commit

---
+++ src/main/java/com/knight/life/KnightApplication.java
@@ -0,0 +1,11 @@
+package com.knight.life;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnightApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(KnightApplication.class, args);
+    }
+}
---
+++ src/main/java/com/knight/life/controller/PageController.java
@@ -0,0 +1,15 @@
+package com.knight.life.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 首页
+ */
+@Controller
+public class PageController {
+    @RequestMapping("/")
+    public String index(){
+        return "main";
+    }
+}
