b38b44284b1205c893cf03ceea76146a0893925c
e <yerongjie1610@163.com>
2019-08-14 14:40:44 +0800

    first commit

--- src/main/java/com/knight/life/controller/PageController.java
+++ src/main/java/com/knight/life/controller/PageController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class PageController {
     @RequestMapping("/")
     public String index(){
-        //
         return "main";
     }
 }
