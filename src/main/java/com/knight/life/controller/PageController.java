7e66d839d93bb51323ff04c13ec672940abdec81
e <yerongjie1610@163.com>
2019-08-14 15:18:59 +0800

    a.jpeg

--- src/main/java/com/knight/life/controller/PageController.java
+++ src/main/java/com/knight/life/controller/PageController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class PageController {
     @RequestMapping("/")
     public String index(){
-        int a = 11;
         return "main";
     }
 }
