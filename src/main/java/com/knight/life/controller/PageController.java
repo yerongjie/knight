d4f21dddc0253a7f0cb69765d0b90a9d2e8a3703
e <yerongjie1610@163.com>
2019-08-14 15:13:05 +0800

    int a= 11

--- src/main/java/com/knight/life/controller/PageController.java
+++ src/main/java/com/knight/life/controller/PageController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class PageController {
     @RequestMapping("/")
     public String index(){
-        int a = 10;
+        int a = 11;
         return "main";
     }
 }
